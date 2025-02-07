3ee74a0e18e944c7ef2070c42c8d3cbfad8ea0b6
iMomobxl <mohamed.alibacha@gmail.com>
2025-02-07 10:23:46 +0100

    add update on localstorage

--- src/js/handleUpdateTodo.js
+++ src/js/handleUpdateTodo.js
@@ -2,5 +2,6 @@ export const handleUpdateTodo = (currentEdit, allTodos, currentEditedItem, setTo
     let newTodo = [...allTodos]
     newTodo[currentEdit] = currentEditedItem
     setTodos(newTodo)
+    localStorage.setItem('todolist', JSON.stringify(newTodo));
     setCurrentEdit("")
 }
